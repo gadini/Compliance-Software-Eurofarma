63ab217acfd5d0259c2755410e129285ecc4388d
gustur0 <alvesrochagadini@gmail.com>
2024-08-25 16:00:49 -0300

    Inicio de projeto backend com frontend

---
+++ src/main/java/com/github/gadini/complainceSoftware/ComplainceSoftwareApplication.java
@@ -0,0 +1,13 @@
+package com.github.gadini.complainceSoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplainceSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplainceSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gadini/complainceSoftware/ComplainceSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gadini.complainceSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComplainceSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
