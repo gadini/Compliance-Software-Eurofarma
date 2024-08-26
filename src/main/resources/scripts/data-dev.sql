898a3868af951b0cc6069c85afd08a306fedaeea
gustur0 <alvesrochagadini@gmail.com>
2024-08-26 13:12:20 -0300

    Adicionando scripts

---
+++ src/main/resources/scripts/data-dev.sql
@@ -0,0 +1,8 @@
+insert into compliances (id, titulo, tipo, data_entrada, corpo)  values (nextval('compliances_seq'), 'TESTE DEV 1', 'TESTE', '2024-08-26', 'TESTE CORPO DEV');
+insert into compliances (id, titulo, tipo, data_entrada, corpo)  values (nextval('compliances_seq'), 'TESTE DEV 2', 'TESTE', '2024-08-26', 'TESTE CORPO DEV 2');
+
+
+
+
+
+
