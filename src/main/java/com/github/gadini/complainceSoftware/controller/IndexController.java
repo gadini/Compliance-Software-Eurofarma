ee33aa56d1f39d87aa397bc162117d3f59973fea
gustur0 <alvesrochagadini@gmail.com>
2024-08-25 22:01:38 -0300

    Configurando pagina index e ambientes de desenvolvimento e producao

--- src/main/java/com/github/gadini/complainceSoftware/controller/IndexController.java
+++ src/main/java/com/github/gadini/complainceSoftware/controller/IndexController.java
@@ -1,5 +1,14 @@
 package com.github.gadini.complainceSoftware.controller;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class IndexController {
+	
+	@GetMapping
+	public String index() {
+		return "index";
+	}
+	
 }
