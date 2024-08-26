1da629f3c7bcf20cbc38d2cf3d76c623cc61e8b1
gustur0 <alvesrochagadini@gmail.com>
2024-08-26 10:30:56 -0300

    Adicionando repository das classes

--- src/main/java/com/github/gadini/complainceSoftware/repository/ComplianceRepository.java
+++ src/main/java/com/github/gadini/complainceSoftware/repository/ComplianceRepository.java
@@ -1,6 +1,10 @@
 package com.github.gadini.complainceSoftware.repository;
 
-public class ComplianceRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.gadini.complainceSoftware.model.Compliance;
+
+public interface ComplianceRepository extends JpaRepository<Compliance, Long>{
 
 	
 }
---
+++ src/main/java/com/github/gadini/complainceSoftware/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package com.github.gadini.complainceSoftware.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.gadini.complainceSoftware.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+}
